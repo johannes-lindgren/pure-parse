880f442e6a8084a7b61419c3fb3672d75f8a5d30
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2024-09-07 19:18:30 +0200

    test: group and rename test

--- packages/pure-parse/src/parse.test.ts
+++ packages/pure-parse/src/parse.test.ts
@@ -335,75 +335,75 @@ describe('parsing', () => {
       })
     })
   })
-  test('with a larger, realistic example', () => {
-    /*
-     * Type aliases
-     */
-    type StringContent = {
-      tag: 'string'
-      value: string
-    }
-    type NumberContent = {
-      tag: 'number'
-      value: number
-    }
-    type UnknownContent = {
-      tag: 'unknown'
-    }
-    type Content = StringContent | NumberContent | UnknownContent
-    type Document = {
-      title: string
-      description?: string
-      content: Content[]
-    }
-    /*
-     * Parsers
-     */
-    const parseStringContent = object<StringContent>({
-      tag: literal('string'),
-      value: parseString,
-    })
-    const parseNumberContent = object<NumberContent>({
-      tag: literal('number'),
-      value: parseNumber,
-    })
-    const parseUnknownContent = object<UnknownContent>({
-      tag: literal('unknown'),
-    })
-    const parseContent = union<[StringContent, NumberContent, UnknownContent]>(
-      parseStringContent,
-      parseNumberContent,
-      parseUnknownContent,
-    )
-    const parseDocument = object<Document>({
-      title: parseString,
-      description: optional(parseString),
-      content: array(fallback(parseContent, { tag: 'unknown' })),
-    })
-    /*
-     * Tests
-     */
-    const data = {
-      title: 'My document',
-      content: [
-        { tag: 'string', value: 'day 1' },
-        // Note that this has a type mismatch error
-        { tag: 'string', value: 2 },
-        { tag: 'number', value: 3 },
-      ],
-    }
-    expect(parseDocument(data)).toEqual({
-      tag: 'success',
-      isSuccess: true,
-      value: {
-        title: data.title,
+  describe('some use cases', () => {
+    test('parsing objects in an array with fallback', () => {
+      /*
+       * Type aliases
+       */
+      type StringContent = {
+        tag: 'string'
+        value: string
+      }
+      type NumberContent = {
+        tag: 'number'
+        value: number
+      }
+      type UnknownContent = {
+        tag: 'unknown'
+      }
+      type Content = StringContent | NumberContent | UnknownContent
+      type Document = {
+        title: string
+        description?: string
+        content: Content[]
+      }
+      /*
+       * Parsers
+       */
+      const parseStringContent = object<StringContent>({
+        tag: literal('string'),
+        value: parseString,
+      })
+      const parseNumberContent = object<NumberContent>({
+        tag: literal('number'),
+        value: parseNumber,
+      })
+      const parseUnknownContent = object<UnknownContent>({
+        tag: literal('unknown'),
+      })
+      const parseContent = union<
+        [StringContent, NumberContent, UnknownContent]
+      >(parseStringContent, parseNumberContent, parseUnknownContent)
+      const parseDocument = object<Document>({
+        title: parseString,
+        description: optional(parseString),
+        content: array(fallback(parseContent, { tag: 'unknown' })),
+      })
+      /*
+       * Tests
+       */
+      const data = {
+        title: 'My document',
         content: [
           { tag: 'string', value: 'day 1' },
-          // Fallback in place
-          { tag: 'unknown' },
+          // Note that this has a type mismatch error
+          { tag: 'string', value: 2 },
           { tag: 'number', value: 3 },
         ],
-      },
+      }
+      expect(parseDocument(data)).toEqual({
+        tag: 'success',
+        isSuccess: true,
+        value: {
+          title: data.title,
+          content: [
+            { tag: 'string', value: 'day 1' },
+            // Fallback in place
+            { tag: 'unknown' },
+            { tag: 'number', value: 3 },
+          ],
+        },
+      })
     })
   })
 })
