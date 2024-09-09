040ab757fe7420cb17343317f4412f7760e77e9f
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2024-09-10 00:01:38 +0200

    fix: Equals utility type (#24)
    The `Equals` utility type doesn't work on type `never` since `never`
    short-circuits the conditional type. The solution is to wrap the type
    parameters inside tuples `[T1]` and `[T2]`.
    
    Also add unit tests for the three utility types:
    
    - `Equals`
    - `OptionalKeys`
    - `RequiredKeys`

--- packages/pure-parse/src/internals/Equals.ts
+++ packages/pure-parse/src/internals/Equals.ts
@@ -1,9 +1,5 @@
-/**
- * Check whether two types are equal. `true` if they are, `false` if not.
- * Useful for testing generics.
- */
-export type Equals<T1, T2> = T1 extends T2
-  ? T2 extends T1
+export type Equals<T1, T2> = [T1] extends [T2]
+  ? [T2] extends [T1]
     ? true
     : false
   : false
