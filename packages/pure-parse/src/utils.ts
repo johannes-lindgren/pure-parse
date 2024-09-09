afa66dd2704b91a4074d8d8839478af26ce5f8b9
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2024-09-10 00:03:15 +0200

    fix: remove duplicate (dead) code

--- packages/pure-parse/src/utils.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Utility functions that are local to this library. They are not exposed.
- */
-
-export const hasKey = <Key extends string>(
-  data: object,
-  key: Key,
-): data is { [K in Key]: unknown } => key in data
