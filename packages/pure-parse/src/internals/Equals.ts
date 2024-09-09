1bf7b821fc09fbcd82106dd25bb93792e9376ad0
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2024-09-09 23:44:01 +0200

    test: Equals utility type

--- packages/pure-parse/src/internals/Equals.ts
+++ packages/pure-parse/src/internals/Equals.ts
@@ -1,9 +1,30 @@
-/**
- * Check whether two types are equal. `true` if they are, `false` if not.
- * Useful for testing generics.
- */
-export type Equals<T1, T2> = T1 extends T2
-  ? T2 extends T1
-    ? true
-    : false
-  : false
+import { describe, it, test } from 'vitest'
+import { Equals } from './Equals.test'
+
+describe('Equals', () => {
+  test('primitives', () => {
+    const a1: Equals<string, string> = true
+    const a2: Equals<string, number> = false
+    const a3: Equals<null, undefined> = false
+  })
+  test('objects', () => {
+    const a1: Equals<{}, {}> = true
+    const a2: Equals<{ a: string }, { a: string }> = true
+    const a3: Equals<{ a: string }, { a: number }> = false
+    const a4: Equals<{ a: string }, {}> = false
+    const a5: Equals<{}, { a: string }> = false
+  })
+  test('tuples', () => {
+    const a0: Equals<[], []> = true
+    const a2: Equals<[number, number], [number, number]> = true
+    const a3: Equals<[number], [number, number]> = false
+    const a4: Equals<[number, number], [number]> = false
+  })
+  test('arrays', () => {
+    const a0: Equals<[], []> = true
+    const a1: Equals<string[], []> = false
+    const a2: Equals<(string | number)[], (string | number)[]> = true
+    const a3: Equals<(string | number)[], string[]> = false
+    const a4: Equals<string[], (string | number)[]> = false
+  })
+})
