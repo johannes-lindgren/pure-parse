86be7a649f3b40ebdae135110b21348769a46acc
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2024-10-06 18:16:03 +0200

    docs: jsdocs

--- packages/pure-parse/src/common/infer.ts
+++
@@ -1,8 +0,0 @@
-import { Parser } from '../parse'
-import { Validator } from '../validate'
-
-/**
- * Extract the type in the [type predicate](https://www.typescriptlang.org/docs/handbook/advanced-types.html#using-type-predicates).
- */
-export type Infer<T extends Validator<unknown> | Parser<unknown>> =
-  T extends Parser<infer R> ? R : T extends Validator<infer R> ? R : never
