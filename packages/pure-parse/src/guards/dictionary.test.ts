2f73927294c4929642de053420395b3df00d82a8
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2024-11-19 22:22:02 +0100

    feat!: dictionaries (#97)
    Resolves #30
    
    - remove record parser
    - rename partialRecordGuard -> dictionaryGuard
    - add dictionary parser

---
+++ packages/pure-parse/src/guards/dictionary.test.ts
@@ -0,0 +1,123 @@
+import { describe, expect, it, test } from 'vitest'
+import { dictionaryGuard } from './dictionary'
+import { isBoolean, isNumber, isString } from './primitives'
+import { Guard } from './types'
+import { literalGuard } from './literal'
+import { objectGuard } from './object'
+import { oneOfGuard } from './oneOf'
+import { arrayGuard } from './arrays'
+import { Equals } from '../internals'
+import { Infer } from '../common'
+
+describe('dictionaries', () => {
+  describe('type checking', () => {
+    it('returns a guard', () => {
+      dictionaryGuard(isString, isString) satisfies Guard<
+        Partial<Record<string, string>>
+      >
+      dictionaryGuard(isString, isNumber) satisfies Guard<
+        Partial<Record<string, number>>
+      >
+      // @ts-expect-error
+      dictionaryGuard(isString, isString) satisfies Guard<
+        Partial<Record<string, number>>
+      >
+    })
+    describe('type inference', () => {
+      it('infers', () => {
+        const guard = dictionaryGuard(isString, isNumber)
+        const t0: Equals<
+          Infer<typeof guard>,
+          Partial<Record<string, number>>
+        > = true
+      })
+    })
+    describe('explicit generic type annotation', () => {
+      test('string as key', () => {
+        dictionaryGuard<string, string>(isString, isString)
+        dictionaryGuard<string, number[]>(isString, arrayGuard(isNumber))
+        // @ts-expect-error
+        dictionaryGuard<string, string>(isString, isNumber)
+        // @ts-expect-error
+        dictionaryGuard<string, number[]>(isString, arrayGuard(isString))
+      })
+      test('literal union as key', () => {
+        dictionaryGuard<'a', string>(literalGuard('a'), isString)
+        // @ts-expect-error
+        dictionaryGuard<'a', string>(isString, isString)
+        // @ts-expect-error
+        dictionaryGuard<'a', string>(literalGuard('b'), isString)
+      })
+    })
+  })
+  it('validates null', () => {
+    expect(dictionaryGuard(isString, isString)(null)).toEqual(false)
+  })
+  it('validates undefined', () => {
+    expect(dictionaryGuard(isString, isString)(undefined)).toEqual(false)
+  })
+  it('validates empty objects', () => {
+    expect(dictionaryGuard(isString, isString)({})).toEqual(true)
+  })
+  it('invalidates empty arrays', () => {
+    expect(dictionaryGuard(isString, isString)([])).toEqual(false)
+  })
+  it('invalidates arrays', () => {
+    expect(dictionaryGuard(isString, isString)(['a'])).toEqual(false)
+  })
+  it('validates objects where the type of the keys match', () => {
+    expect(
+      dictionaryGuard(isString, isString)({ a: 'hello', b: 'hello2' }),
+    ).toEqual(true)
+    expect(dictionaryGuard(isString, isNumber)({ a: 1, b: 1 })).toEqual(true)
+    expect(dictionaryGuard(isString, isBoolean)({ a: true, b: false })).toEqual(
+      true,
+    )
+  })
+  it('invalidates objects where the type of any key does not match', () => {
+    expect(dictionaryGuard(isString, isString)({ a: 'hello', b: 1 })).toEqual(
+      false,
+    )
+    expect(dictionaryGuard(isString, isNumber)({ a: 'hello', b: 1 })).toEqual(
+      false,
+    )
+    expect(
+      dictionaryGuard(isString, isBoolean)({ a: 'hello', b: true }),
+    ).toEqual(false)
+  })
+  test('extra properties in the schema', () => {
+    const isObj = objectGuard({
+      a: isNumber,
+      // @ts-expect-error - all properties must be guard functions
+      b: 123,
+    })
+  })
+  describe('keys', () => {
+    it('allows keys to be of type string', () => {
+      expect(
+        dictionaryGuard(isString, isString)({ a: 'hello', b: 'hello2' }),
+      ).toEqual(true)
+      expect(dictionaryGuard(isString, isString)({})).toEqual(true)
+    })
+    it('does not allow extra keys', () => {
+      const isKey = oneOfGuard(literalGuard('a'), literalGuard('b'))
+      expect(
+        dictionaryGuard(isKey, isString)({ a: 'hello', b: 'hello2' }),
+      ).toEqual(true)
+      expect(
+        dictionaryGuard(
+          isKey,
+          isString,
+        )({ a: 'hello', b: 'hello2', c: 'hello3' }),
+      ).toEqual(false)
+    })
+    it('allows each key to be omitted', () => {
+      const isKey = oneOfGuard(literalGuard('a'), literalGuard('b'))
+      expect(
+        dictionaryGuard(isKey, isString)({ a: 'hello', b: 'hello2' }),
+      ).toEqual(true)
+      expect(dictionaryGuard(isKey, isString)({ a: 'hello' })).toEqual(true)
+      expect(dictionaryGuard(isKey, isString)({})).toEqual(true)
+    })
+  })
+})
---
+++ packages/pure-parse/src/parsers/dictionary.test.ts
@@ -0,0 +1,251 @@
+import { describe, it, expect, test } from 'vitest'
+import { dictionary } from './dictionary'
+import {
+  parseBoolean,
+  parseNumber,
+  parseString,
+  parseSymbol,
+} from './primitives'
+import { literal } from './literal'
+import { Equals } from '../internals'
+import { Infer } from '../common'
+import { failure, Parser, success } from './types'
+import { isString } from '../guards'
+
+const parseCapitalized: Parser<string> = (data) => {
+  if (!isString(data)) {
+    return failure('Expected a string')
+  }
+  return success(data.toUpperCase())
+}
+
+describe('record', () => {
+  it('invalidates non-objects', () => {
+    ;[1, 'a', true, null, undefined].forEach((data) => {
+      expect(dictionary(parseString, parseBoolean)(data)).toEqual(
+        expect.objectContaining({ tag: 'failure' }),
+      )
+    })
+  })
+  describe('types', () => {
+    describe('type inference', () => {
+      it('infers the keys as string', () => {
+        const parse = dictionary(parseString, parseString)
+        const t0: Equals<
+          Infer<typeof parse>,
+          Partial<Record<string, string>>
+        > = true
+      })
+      it('infers the keys as string literal', () => {
+        const parse = dictionary(literal('a', 'b'), parseString)
+        const t0: Equals<
+          Infer<typeof parse>,
+          Partial<Record<'a' | 'b', string>>
+        > = true
+      })
+      it('infers the value', () => {})
+    })
+    describe('explicit type declaration', () => {
+      it('requires two type arguments', () => {
+        dictionary<string, string>(parseString, parseString)
+      })
+      it('allows for union keys', () => {
+        dictionary<'a' | 'b', string>(literal('a', 'b'), parseString)
+      })
+      it('binds the second type argument to the second parser', () => {
+        dictionary<string, string>(parseString, parseString)
+        // @ts-expect-error
+        dictionary<string, string>(parseString, parseNumber)
+        // @ts-expect-error
+        dictionary<string, number>(parseString, parseString)
+        dictionary<string, number>(parseString, parseNumber)
+      })
+    })
+  })
+  describe('keys', () => {
+    describe('string', () => {
+      it('parses empty objects', () => {
+        expect(dictionary(parseString, parseBoolean)({})).toEqual(
+          expect.objectContaining({ tag: 'success' }),
+        )
+      })
+      it('parses objects with string keys', () => {
+        expect(
+          dictionary(
+            parseString,
+            parseBoolean,
+          )({
+            a: true,
+          }),
+        ).toEqual(expect.objectContaining({ tag: 'success' }))
+      })
+      it('allows objects with non-string keys', () => {
+        expect(
+          dictionary(
+            parseString,
+            parseBoolean,
+          )({
+            [Symbol()]: true,
+          }),
+        ).toEqual(expect.objectContaining({ tag: 'success' }))
+      })
+    })
+    test('number', () => {
+      // @ts-expect-error -- can't use number, because numbers are coerced to strings
+      dictionary(parseNumber, parseBoolean)
+    })
+    test('symbol', () => {
+      // @ts-expect-error -- can't use symbol
+      dictionary(parseSymbol, parseBoolean)
+    })
+    describe('unions', () => {
+      it('parses unions', () => {
+        const t0: { a: boolean; b: boolean } = { a: true, b: true }
+        expect(
+          dictionary(
+            literal('a', 'b'),
+            parseBoolean,
+          )({
+            a: true,
+            b: true,
+          }),
+        ).toEqual(
+          expect.objectContaining({
+            tag: 'success',
+            value: {
+              a: true,
+              b: true,
+            },
+          }),
+        )
+      })
+      it('fails on extra keys', () => {
+        expect(
+          dictionary(
+            literal('a', 'b'),
+            parseBoolean,
+          )({
+            a: true,
+            b: true,
+            c: true,
+          }),
+        ).toEqual(expect.objectContaining({ tag: 'failure' }))
+      })
+
+      test('that keys are optional', () => {
+        expect(
+          dictionary(
+            literal('a', 'b'),
+            parseBoolean,
+          )({
+            a: true,
+          }),
+        ).toEqual(
+          expect.objectContaining({ tag: 'success', value: { a: true } }),
+        )
+      })
+    })
+
+    test('transformations', () => {
+      const parseCapitalized: Parser<string> = (data) => {
+        if (!isString(data)) {
+          return failure('Expected a string')
+        }
+        return success(data.toUpperCase())
+      }
+      const parse = dictionary(parseCapitalized, parseNumber)
+      expect(parse({ a: 1 })).toEqual(
+        expect.objectContaining({ tag: 'success', value: { A: 1 } }),
+      )
+    })
+  })
+  describe('values', () => {
+    it('parses empty objects', () => {
+      expect(dictionary(parseString, parseBoolean)({})).toEqual(
+        expect.objectContaining({ tag: 'success', value: {} }),
+      )
+    })
+    it('parses objects with string values', () => {
+      expect(dictionary(parseString, parseString)({ a: 'a' })).toEqual(
+        expect.objectContaining({
+          tag: 'success',
+          value: {
+            a: 'a',
+          },
+        }),
+      )
+    })
+    it('parses objects with boolean values', () => {
+      expect(dictionary(parseString, parseBoolean)({ a: true })).toEqual(
+        expect.objectContaining({
+          tag: 'success',
+          value: {
+            a: true,
+          },
+        }),
+      )
+    })
+    it('invalidates values that do not pass validation', () => {
+      expect(dictionary(parseString, parseBoolean)({ a: 'a' })).toEqual(
+        expect.objectContaining({ tag: 'failure' }),
+      )
+    })
+    test('transformations', () => {
+      const parse = dictionary(parseString, parseCapitalized)
+      expect(parse({ a: 'a' })).toEqual(
+        expect.objectContaining({ tag: 'success', value: { a: 'A' } }),
+      )
+    })
+  })
+  describe('error handling', () => {
+    test('that the error points to the object when it is not an object', () => {
+      expect(dictionary(parseString, parseBoolean)(1)).toEqual(
+        expect.objectContaining({ tag: 'failure', path: [] }),
+      )
+    })
+    describe('property errors', () => {
+      test('keys', () => {
+        test('values', () => {
+          expect(
+            dictionary(
+              literal('a'),
+              parseBoolean,
+            )({
+              b: true,
+            }),
+          ).toEqual(
+            expect.objectContaining({
+              tag: 'failure',
+              path: [
+                {
+                  tag: 'object',
+                  key: 'a',
+                },
+              ],
+            }),
+          )
+        })
+      })
+      test('values', () => {
+        expect(
+          dictionary(
+            parseString,
+            parseBoolean,
+          )({
+            a: 1,
+          }),
+        ).toEqual(
+          expect.objectContaining({
+            tag: 'failure',
+            path: [
+              {
+                tag: 'object',
+                key: 'a',
+              },
+            ],
+          }),
+        )
+      })
+    })
+  })
+})
