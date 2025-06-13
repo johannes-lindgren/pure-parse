1bb9f34e64f46af45e8fe8dc914f149c51283123
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2025-06-13 15:00:22 +0200

    fix: export json modules (#141)

---
+++ packages/test-app/src/exports.ts
@@ -0,0 +1,52 @@
+// Tests that everthing is exported
+
+// Primitives
+import {
+  parseString,
+  parseNumber,
+  parseBoolean,
+  parseBigInt,
+  parseSymbol,
+  parseNull,
+  parseUndefined,
+  parseUnknown,
+  parseNever,
+  isString,
+  isNumber,
+  isBoolean,
+  isBigInt,
+  isSymbol,
+  isArray,
+  isObject,
+  isUndefined,
+  isNull,
+  isUnknown,
+  isNever,
+} from 'pure-parse'
+
+// JSON
+import { JsonValue, parseJson, isJsonValue } from 'pure-parse'
+
+// Higher order parsers
+import {
+  array,
+  object,
+  oneOf,
+  optional,
+  withDefault,
+  equals,
+  map,
+  chain,
+  recover,
+} from 'pure-parse'
+
+// Higher order guards
+import {
+  equalsGuard,
+  objectGuard,
+  arrayGuard,
+  optionalGuard,
+} from 'pure-parse'
+
+// @ts-expect-error -- test that importing something that doesn't exist gives an error
+import { abcSomethingThatDoesNotExist } from 'pure-parse'
