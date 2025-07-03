a00eb7fdd48f4dee1654604f7efd3386d439c644
Johannes Lindgren <14206504+johannes-lindgren@users.noreply.github.com>
2025-07-03 13:32:38 +0200

    docs: recursive types (#143)
    Document how to use `lazy` to define recursive parsers

--- packages/pure-parse/src/common/lazy.test.ts
+++ packages/pure-parse/src/common/lazy.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it, test, vi } from 'vitest'
 import { lazy } from './lazy'
 import { Equals } from '../internals'
+import { object, optional, Parser, parseString, success } from '../parsers'
 
 describe('lazy', () => {
   it('only constructs the function once (memoization)', () => {
@@ -25,5 +26,133 @@ describe('lazy', () => {
 
     const fn3 = lazy(() => (base: number, exp: number) => base ** exp)
     const t3: Equals<typeof fn3, (base: number, exp: number) => number> = true
+  })
+  it('supports direct recursive parsers', () => {
+    type Person = {
+      name: string
+      mother?: Person
+      father?: Person
+    }
+    const parsePerson: Parser<Person> = lazy(() =>
+      object({
+        name: parseString,
+        mother: optional(parsePerson),
+        father: optional(parsePerson),
+      }),
+    )
+
+    // Test that it works in runtime, even though it is the types that matter
+    const person1: Person = {
+      name: 'Johannes',
+    }
+    expect(parsePerson(person1)).toEqual(success(person1))
+
+    const person2: Person = {
+      name: 'Bob',
+      mother: {
+        name: 'Alice',
+      },
+      father: {
+        name: 'Evan',
+      },
+    }
+    expect(parsePerson(person2)).toEqual(success(person2))
+
+    const person3: Person = {
+      name: 'Charlie',
+      mother: {
+        name: 'Diana',
+        mother: {
+          name: 'Diana',
+        },
+        father: {
+          name: 'Evan',
+        },
+      },
+      father: {
+        name: 'Johan',
+        mother: {
+          name: 'Alice',
+        },
+        father: {
+          name: 'Frank',
+        },
+      },
+    }
+    expect(parsePerson(person3)).toEqual(success(person3))
+
+    // Failure cases
+    const person4 = {
+      name: 'A',
+      mother: {
+        name: 'B',
+        father: {}, // Missing required field 'name'
+      },
+    }
+    expect(parsePerson(person4)).toEqual(
+      expect.objectContaining({
+        tag: 'failure',
+      }),
+    )
+  })
+  it('supports indirect recursive parsers', () => {
+    type Family = {
+      mother: Person
+      father: Person
+    }
+    type Person = {
+      family?: Family
+    }
+    const parsePerson: Parser<Person> = lazy(() =>
+      object({
+        family: optional(parseFamily),
+      }),
+    )
+    const parseFamily: Parser<Family> = lazy(() =>
+      object({
+        mother: parsePerson,
+        father: parsePerson,
+      }),
+    )
+
+    // Test that it works in runtime, even though it is the types that matter
+    const person1: Person = {}
+    expect(parsePerson(person1)).toEqual(success(person1))
+
+    const person2: Person = {
+      family: {
+        mother: {},
+        father: {},
+      },
+    }
+    expect(parsePerson(person2)).toEqual(success(person2))
+
+    const person3: Person = {
+      family: {
+        mother: {
+          family: {
+            mother: {},
+            father: {},
+          },
+        },
+        father: {
+          family: {
+            mother: {},
+            father: {},
+          },
+        },
+      },
+    }
+    expect(parsePerson(person3)).toEqual(success(person3))
+
+    // Failure cases
+    const person4 = {
+      family: {},
+    }
+    expect(parsePerson(person4)).toEqual(
+      expect.objectContaining({
+        tag: 'failure',
+      }),
+    )
   })
 })
